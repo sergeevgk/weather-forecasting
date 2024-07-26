c3e8979a8cf83dbfd0342c6b78958b20ab374ac3
sergeevgk <sergeevgk.srb@gmail.com>
2024-07-26 16:36:13 +0300

    Implement main requirements: Implement draft WeatherForecastController with mock parameters.

--- WeatherForecasting.WebApi/Controllers/WeatherForecastController.cs
+++ WeatherForecasting.WebApi/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+using WeatherForecasting.WebApi.Models.Response;
 
 namespace WeatherForecasting.WebApi.Controllers
 {
@@ -6,28 +10,41 @@ namespace WeatherForecasting.WebApi.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
-
 		private readonly ILogger<WeatherForecastController> _logger;
+		private readonly HttpClient _client;
+		private readonly OpenWeatherMapSettings _settings;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory, IOptions<OpenWeatherMapSettings> settings)
 		{
-			_logger = logger;
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			_client = httpClientFactory?.CreateClient("WeatherForecastClient") ?? throw new ArgumentNullException(nameof(httpClientFactory));
+			_settings = settings.Value;
 		}
 
 		[HttpGet(Name = "GetWeatherForecast")]
-		public IEnumerable<WeatherForecast> Get()
+		public async Task<WeatherForecastResponse> GetWeatherForecast()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			_logger.LogInformation("Requesting forecast");
+			var query = BuildRequestQuery(10, 10, _settings.ApiKey);
+			var response = await _client.GetAsync(query);
+
+			response.EnsureSuccessStatusCode();
+
+			var content = await response.Content.ReadAsStringAsync();
+			return JsonSerializer.Deserialize<WeatherForecastResponse>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+		}
+
+		private string BuildRequestQuery(decimal latitude, decimal longitude, string apiKey)
+		{
+			var queryParameterDictionary = new Dictionary<string, string>()
 			{
-				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-			})
-			.ToArray();
+				{ "lat", latitude.ToString("N2") },
+				{ "lon", longitude.ToString("N2") },
+				{ "appid", apiKey }
+
+			};
+			var result = QueryHelpers.AddQueryString("", queryParameterDictionary);
+			return result;
 		}
 	}
 }
