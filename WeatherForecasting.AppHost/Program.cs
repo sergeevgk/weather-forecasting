921422996bbf672b2c89eb9acaa81f86bf61634b
sergeevgk <sergeevgk.srb@gmail.com>
2024-08-13 00:13:31 +0300

    Add Angular frontend client project as NpmApp in Aspire. Provide the port settings and the reference to weather api service to use its environment variable for URL.

--- WeatherForecasting.AppHost/Program.cs
+++ WeatherForecasting.AppHost/Program.cs
@@ -2,6 +2,13 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.WeatherForecasting_WebApi>("apiservice")
-						.WithReference(cache);
+						.WithReference(cache)
+						.WithExternalHttpEndpoints();
+
+var angularClient = builder.AddNpmApp("AngularFrontEnd", "../frontend-clients/weather-angular-client")
+						.WithReference(apiService)
+						.WithHttpEndpoint(port: 4200, env: "PORT", name: "http")
+						.WithExternalHttpEndpoints()
+						.PublishAsDockerFile();
 
 builder.Build().Run();
