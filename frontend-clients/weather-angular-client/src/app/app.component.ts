e608169184a42ee8076af52d820477bc920e135e
sergeevgk <sergeevgk.srb@gmail.com>
2024-08-25 22:34:53 +0300

    1. Use angular material design library. 2. Add a selector for city in the app.component and pass the value to the api request. 3. Move the http request to private function and call it on changing selector value. TODO: add date selector, move weather forecast to a separate component.

--- frontend-clients/weather-angular-client/src/app/app.component.ts
+++ frontend-clients/weather-angular-client/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { CommonModule, formatDate } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { WeatherForecast, WeatherForecasts } from '../types/weatherForecast';
@@ -9,22 +13,34 @@ import { WeatherForecastResponse } from '../types/weatherForecastResponse';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatInputModule, MatSelectModule, MatFormFieldModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  cityName: string = "London";
   title = 'Weather';
   forecasts: WeatherForecasts = [];
+  cityList: string[] = [
+    "London", "Paris", "Belgrade", "Vladivostok", "Washington", "New York", "Tokyo", "Dubai", "Rome", "Moscow", "Sydney"
+  ];
 
-  constructor(private http: HttpClient, @Inject(LOCALE_ID) private locale: string) {
+  constructor(private httpClient: HttpClient, @Inject(LOCALE_ID) private locale: string) {
+    this.FetchForecasts();
+  }
+
+  onSelectNewCity(){
+    this.FetchForecasts();
+  }
+
+  private FetchForecasts(){
     let currentDateStr = new Date();
     let tomorrowDate = currentDateStr.setDate(currentDateStr.getDate() + 1);
     let tomorrowDateStr = formatDate(tomorrowDate, 'yyyy-MM-dd', "en-US");
-    http.get<WeatherForecastResponse>(`/api/weatherforecast?city=London&date=${tomorrowDateStr}`).subscribe({
+    this.httpClient.get<WeatherForecastResponse>(`/api/weatherforecast?city=${this.cityName}&date=${tomorrowDateStr}`).subscribe({
       next: result => this.forecasts = result.forecasts.map(
         f => new WeatherForecast(
-          formatDate(f.localTime, 'yyyy-MM-dd HH:mm', locale), 
+          formatDate(f.localTime, 'yyyy-MM-dd HH:mm', this.locale), 
           f.weatherSummary.temperature.value, 
           +(f.weatherSummary.temperature.value * 9/5 + 32).toFixed(2),
         f.weatherSummary.description)),
