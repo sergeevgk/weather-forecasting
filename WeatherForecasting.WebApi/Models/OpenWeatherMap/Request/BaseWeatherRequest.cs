d7e6d0534d662931059144120327148086187a5c
sergeevgk <sergeevgk.srb@gmail.com>
2024-08-25 22:31:28 +0300

    Use 'metric' system by default in weather api requests.

--- WeatherForecasting.WebApi/Models/OpenWeatherMap/Request/BaseWeatherRequest.cs
+++ WeatherForecasting.WebApi/Models/OpenWeatherMap/Request/BaseWeatherRequest.cs
@@ -2,12 +2,15 @@
 
 public record BaseWeatherRequest(decimal Latitude, decimal Longitude) : IQueryParametersRequest
 {
+	private const string DEFAULT_UNIT_SYSTEM = "metric";
+
 	public virtual Dictionary<string, string> ToQueryParametersDictionary()
 	{
 		var result = new Dictionary<string, string>()
 		{
 			{ "lat", Latitude.ToString("N2") },
-			{ "lon", Longitude.ToString("N2") }
+			{ "lon", Longitude.ToString("N2") },
+			{ "units", DEFAULT_UNIT_SYSTEM }
 		};
 
 
