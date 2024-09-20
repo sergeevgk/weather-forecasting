07091389c77c0b2be7651812a7402f84f410b496
sergeevgk <sergeevgk.srb@gmail.com>
2024-09-20 22:10:28 +0300

    Add json-server and set up a new start option with fake backend api. Use cross-env to set up an environment variable for any environment (Windows and Linux use different ways).

--- frontend-clients/weather-angular-client/src/proxy.conf.js
+++ frontend-clients/weather-angular-client/src/proxy.conf.js
@@ -1,6 +1,8 @@
+var jsonServerTarget = "http://localhost:3000";
+var defaultTarget = process.env["services__apiservice__http__0"];
 module.exports = {
   "/api": {
-    target: process.env["services__apiservice__http__0"],
+    target: process.env["FAKE_BACKEND"] ? jsonServerTarget : defaultTarget,
     pathRewrite: {
       "^/api": "",
     },
